a357b0794315dbae06b1719588df8905df8d9749
Hector Garcia <garcia27h@hotmail.com>
2023-12-24 14:40:17 -0300

    Requerimiento 1

---
+++ react 3/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import AlertComponent from './components/Alert'
+import Buscador from './components/Buscador'
+import Formulario from './components/Formulario'
+import Listado from './components/Listado'
+
+function App() {
+
+  return (
+    <>
+      <Buscador/>
+      <Listado/>
+      <AlertComponent/>
+      <Formulario/>
+    </>
+  )
+}
+
+export default App
---
+++ react 3/src/components/Alert.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AlertComponent = () => {
+  return (
+    <div>Alert</div>
+  )
+}
+
+export default AlertComponent
