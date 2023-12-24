057a24993d6a78f1da794045f6b2a26ffb539d5b
Jhonny Quevedo <quevedojhonny@gmail.com>
2023-12-24 15:55:01 -0300

    Requerimiento 4

--- react 3/src/App.jsx
+++ react 3/src/App.jsx
@@ -3,18 +3,20 @@ import AlertComponent from './components/Alert'
 import Buscador from './components/Buscador'
 import Formulario from './components/Formulario'
 import Listado from './components/Listado'
-import { baseColaboradores } from './baseColaboradores'
+import { baseColaboradores } from './components/baseColaboradores'
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   const [lista, setLista] = useState(baseColaboradores)
+  const [error, setError] = useState("")
+  const [succes, setSucces] = useState("")
   return (
     <>
       <Buscador/>
       <Listado lista={lista}/>
       <AlertComponent/>
-      <Formulario/>
+      <Formulario setLista={setLista} lista={lista} setError={setError} setSucces={setSucces}/>
     </>
   )
 }
--- react 3/src/components/Formulario.jsx
+++ react 3/src/components/Formulario.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
 
-const Formulario = () => {
-  return (
-    <div>Formulario</div>
-  )
+const Formulario = ({ lista, setLista, setError, setSucces }) => {
+  const [form, setForm] = useState({})
+    
+    const agregarColaborador = (evento) => {
+        evento.preventDefault()
+        if (!form.nombre || !form.correo || !form.edad || !form.cargo || !form.telefono) {
+            setError("Los campos no pueden estar vacios")
+            console.log("error")
+            setSucces("")
+            return
+        } else {
+            const nuevaBase = [...lista, form];
+            setLista(nuevaBase)
+            setForm({ id: "", nombre: "", correo: "", edad: "", cargo: "", telefono: "" })
+            setError("")
+            setSucces("El Colaborador se ha agregado con exito")
+        }
+    }
+
+    const capturaInput = (evento) => {
+        setForm({ ...form, [evento.target.name]: evento.target.value })
+        console.log(form)
+    }
+    return (
+        <form className='d-flex flex-column gap-1' onSubmit={agregarColaborador}>
+            <input onChange={capturaInput} name='nombre' value={form.nombre} type="text" placeholder='Nombre' />
+            <input onChange={capturaInput} name='correo' value={form.correo} type="text" placeholder='tucorreo@example.com' />
+            <input onChange={capturaInput} name='edad' value={form.edad} type="text" placeholder='Edad' />
+            <input onChange={capturaInput} name='cargo' value={form.cargo} type="text" placeholder='Cargo' />
+            <input onChange={capturaInput} name='telefono' value={form.telefono} type="text" placeholder='Telefono' />
+            <Button variant="outline-success" type='submit'>Agregar colaborador</Button>
+        </form>
+    )
 }
 
 export default Formulario
+
